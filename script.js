0cf627df6936ee1f91ac13d952fb03d9189e43e4
Thiago Silva Hatanaka <thiagohatanaka18@gmail.com>
2023-12-02 17:15:23 -0300

    Objeto
    Criando um array com objetos com propriedades como valores booleanos e respostas.

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-alert("ola")
+const questions = [
+    {
+        question: "Which is larget animal in the world ?",
+        answers: [
+            { text: "Shark", correct: false},
+            { text: "Blue whale", correct: true},
+            { text: "Elephant", correct: false},
+            { text: "Giraffe", correct: false},
+        ]
+    },
+    {
+        question: "Which is the smallest country in the world ?",
+        answers: [
+            { text: "Vatican", correct: false},
+            { text: "Bhutan", correct: false},
+            { text: "Nepal", correct: false},
+            { text: "Shri Lanka", correct: true},
+        ]
+    },
+    {
+        question: "Which is larget animal in the world ?",
+        answers: [
+            { text: "Kalahari", correct: true},
+            { text: "Gobi", correct: false},
+            { text: "Sahara", correct: false},
+            { text: "Antartica", correct: false},
+        ]
+    },
+]
