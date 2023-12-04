a569a832aacf6065c2ec0c0baea3520554e48ea8
Thiago Silva Hatanaka <thiagohatanaka18@gmail.com>
2023-12-04 11:00:46 -0300

    End
    Project end

--- script.js
+++ script.js
@@ -27,10 +27,13 @@ const questions = [
         ]
     },
 ];
+// Criando uma const onde ira receber um array, com um objeto em cada índice, com duas propriedades
+// sendo uma propriedade a pergunta, outra propriedade um array novamente contendo as repostas junto aos valores booleanos
 
 const questionElement = document.getElementById("question");
 const answerButton = document.getElementById("answer-buttons");
 const nextButton = document.getElementById("next-btn");
+// Pegando os elementos do html para manipula-los
 
 let currentQuestionIndex = 0;
 let score = 0;
@@ -41,15 +44,76 @@ function startQuiz(){
     nextButton.innerHTML = "Next";
     showQuestion();
 }
+// Função com os valores iniciais, chamando a função "showQuestion"
+
 function showQuestion(){
+    resetState();
     let currentQuestion = questions[currentQuestionIndex];
-    let questionNo = currentQuestion + 1;
+    let questionNo = currentQuestionIndex + 1;
     questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
+// question é um array com objetos, currentQuestion recebe o objeto de acordo com o indice
+// como o comelo é o indice 0 para representar o nº vai ser somado com 1
+// questionElement ira alterar o html adicionando o numero e o string da pergunta
 
     currentQuestion.answers.forEach(answer => {
-        const button = document.CreateElement("button");
+        const button = document.createElement("button");
         button.innerHTML = answer.text;
         button.classList.add("btn");
         answerButton.appendChild(button);
-    })
+        if(answer.correct){
+            button.dataset.correct = answer.correct;
+        }
+        button.addEventListener("click", selectAnswer);
+    });
 }
+// currentQuestion possui o objeto "question" acessamos a propriedade answers que contem um array de objetos
+// e chamamos um loop para cada elemento com o forEach
+startQuiz()
+
+function resetState(){
+    nextButton.style.display = "none";
+    while(answerButton.firstChild){
+        answerButton.removeChild(answerButton.firstChild);
+    }
+}
+function selectAnswer(e){
+    const selectedBtn = e.target;
+    const isCorrect = selectedBtn.dataset.correct === "true";
+    if(isCorrect){
+        selectedBtn.classList.add("correct");
+        score++;
+    }else{
+        selectedBtn.classList.add("incorrect");
+    }
+    Array.from(answerButton.children).forEach(button => {
+        if(button.dataset.correct === "true"){
+            button.classList.add("correct");
+        }
+        button.disabled = true;
+    });
+    nextButton.style.display = "block"
+}
+
+function showScore(){
+    resetState();
+    questionElement.innerHTML = `You scored ${score} out of ${questions.length}!`;
+    nextButton.innerHTML = "Play Again";
+    nextButton.style.display = "block";
+}
+
+function handleNextButton(){
+    currentQuestionIndex++;
+    if(currentQuestionIndex < questions.length){
+        showQuestion();
+    }else{
+        showScore();
+    }
+}
+
+nextButton.addEventListener("click", () =>{
+    if(currentQuestionIndex < questions.length){
+        handleNextButton();
+    }else{
+        startQuiz();
+    }
+});
